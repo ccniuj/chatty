9215a2442789f70dfaeec9e883a5a0d524ae0eec
Juin Chiu <davidjuin0519@gmail.com>
2016-01-30 14:11:19 +0800

    show user selfie

--- config/initializers/websocket.rb
+++ config/initializers/websocket.rb
@@ -8,16 +8,11 @@ class ChatController
 
   # the index will answer '/'
   # a regular method will answer it's own name i.e. '/foo'
-  def foo
-    "Bar!"
-  end
-  # show is RESTful, it will answer any request looking like: '/(:id)'
-  def show
-    "Are you looking for: #{params[:id]}?"
+  def before
+    @current_user = get_user
   end
 
   def index
-    @current_user = get_user
     response['content-type'] = 'text/html'
     render :chat
   end
@@ -30,7 +25,7 @@ class ChatController
       response.close
       return false
     end
-    broadcast :_send_message, {event: :chat, from: params[:id], message: data["message"], at: Time.now}.to_json
+    broadcast :_send_message, {event: :chat, from: params[:id], message: data["message"], selfie_url: @current_user.selfie_url, at: Time.now}.to_json
   end
 
   def _send_message data
@@ -44,7 +39,6 @@ class ChatController
       return false
     end
     message = {from: '', at: Time.now}
-    
     # list = collect(:_ask_nickname)
 
     # if ((list.map {|n| n.downcase}) + ['admin', 'system', 'sys', 'administrator']).include? params[:id].downcase
@@ -58,8 +52,10 @@ class ChatController
     # message[:message] = list.empty? ? "You're the first one here." : "#{list[0..-2].join(', ')} #{list[1] ? 'and' : ''} #{list.last} #{list[1] ? 'are' : 'is'} already in the chatroom"
     message[:message] = "hello, #{params[:id]}"
     message[:event] = :chat
+    message[:selfie_url] = @current_user.selfie_url
     response << message.to_json
     message[:message] = "#{params[:id]} joined the chatroom."
+    # binding.pry
     broadcast :_send_message, message.to_json
   end
 
