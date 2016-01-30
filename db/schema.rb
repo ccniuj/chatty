eb5a0f99fbfe1317906b492ec68dbc87bb232221
Juin Chiu <davidjuin0519@gmail.com>
2016-01-30 13:05:48 +0800

    add user selfie

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20160127153524) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
+    t.string   "selfie_url"
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
