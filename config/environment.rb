e6fceb6769c415174382c859c7ac610d1601c1f3
Juin Chiu <davidjuin0519@gmail.com>
2016-01-31 21:05:21 +0800

    fix deploy

--- config/environment.rb
+++ config/environment.rb
@@ -3,10 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-module OpenSSL
-   module SSL
-       SSLErrorWaitReadable = IO::WaitReadable
-   end
-end
-require 'openssl'
