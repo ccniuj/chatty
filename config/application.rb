52866d143077320ddfacf8776e77576f9c532cb2
Juin Chiu <davidjuin0519@gmail.com>
2016-02-01 10:33:03 +0800

    fix deploy

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'openssl'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -17,7 +18,7 @@ end
 #        SSLErrorWaitReadable = IO::WaitReadable
 #    end
 # end
-# require 'openssl'
+
 
 module Chatty
   class Application < Rails::Application
